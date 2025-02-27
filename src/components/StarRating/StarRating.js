8be6a3e98cb378e7e0fd2e2d3dacdffe7f7c89f7
kaveh1985 <kaveh.ramzanzad@gmail.com>
2025-02-28 00:56:44 +0100

    new improvement

--- src/components/StarRating/StarRating.js
+++ src/components/StarRating/StarRating.js
@@ -1,33 +1,32 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import classes from './StarRating.module.css';
-export default function StarRating({ stars, size }) {
-  const styles = {
-    width: size + 'px',
-    height: size + 'px',
-    marginRight: size / 6 + 'px',
-  };
 
-  function Star({ number }) {
-    const halfNumber = number - 0.5;
-
-    return stars >= number ? (
-      <img src="/star-full.svg" style={styles} alt={number} />
-    ) : stars >= halfNumber ? (
-      <img src="/star-half.svg" style={styles} alt={number} />
-    ) : (
-      <img src="/star-empty.svg" style={styles} alt={number} />
-    );
-  }
-
-  return (
-    <div className={classes.rating}>
-      {[1, 2, 3, 4, 5].map(number => (
-        <Star key={number} number={number} />
-      ))}
-    </div>
+function Star({ number, stars, styles }) {
+  const halfNumber = number - 0.5;
+  return stars >= number ? (
+    <img src="/star-full.svg" style={styles} alt="Full Star" />
+  ) : stars >= halfNumber ? (
+    <img src="/star-half.svg" style={styles} alt="Half Star" />
+  ) : (
+    <img src="/star-empty.svg" style={styles} alt="Empty Star" />
   );
 }
 
-StarRating.defaultProps = {
-  size: 18,
-};
+export default function StarRating({ stars, size }) {
+  // Memoize styles so they only change when 'size' changes.
+  const styles = useMemo(() => ({
+    width: `${size}px`,
+    height: `${size}px`,
+    marginRight: `${size / 6}px`,
+  }), [size]);
+
+  const starElements = useMemo(
+    () =>
+      [1, 2, 3, 4, 5].map((number) => (
+        <Star key={number} number={number} stars={stars} styles={styles} />
+      )),
+    [stars, styles]
+  );
+
+  return <div className={classes.rating}>{starElements}</div>;
+}
